143d89abc0ffdbca677808ff2668cb1e30085ddd
Omar Sanad <omarsanad.me@gmail.com>
2025-05-08 16:20:48 +0300

    Install shadcn packages, Install Button and theme provider for dark mode.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -24,14 +25,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {/* This is the root layout */}
-        {children}
-        About
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          {/* This is the root layout */}
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,30 @@
-import Image from "next/image";
+import {
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  UserButton,
+} from "@clerk/nextjs";
+
+import { Button } from "@/components/ui/button";
 
 const page = () => {
-  return <div>Hello Home Page</div>;
+  return (
+    <div>
+      <SignedOut>
+        <SignInButton mode="modal">
+          <Button variant="outline">Sign In</Button>
+        </SignInButton>
+        <SignUpButton mode="modal">
+          <Button variant="secondary">Sign Up</Button>
+        </SignUpButton>
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
+  );
 };
 
 export default page;
