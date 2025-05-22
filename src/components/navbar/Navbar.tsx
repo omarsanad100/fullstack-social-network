e360bb254af719566f4c32566587b7538dc22a61
Omar Sanad <omarsanad.me@gmail.com>
2025-05-22 16:02:32 +0300

    Save user in database if not exists and update the user if it exists

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-const Navbar = () => {
+const Navbar = async () => {
+  // Save user in database if not exists and update the user if it exists
+  const user = await currentUser();
+  user ? await syncUser() : null;
+  // console.log("user saved in database", user); // debugging
+
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
