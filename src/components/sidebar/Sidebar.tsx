3f93f0d95ce91419acf1ca3220b4288df17d233a
Omar Sanad <omarsanad.me@gmail.com>
2025-05-25 19:43:49 +0300

    Adding card from shadcn

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import { currentUser } from "@clerk/nextjs/server";
 
-const Sidebar = () => {
+const Sidebar = async () => {
+  const authenticatedUser = await currentUser();
+  if (!authenticatedUser) return <unauthenticatedSidebar />;
   return <div>Sidebar</div>;
+
+  const UnAuthenticatedSidebar = () => (
+    <div className="sticky top-20">
+      <Card>
+        <CardHeader>
+          <CardTitle className="text-center text-xl font-semibold">
+            Welcome Back!
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-center text-muted-foreground mb-4">
+            Login to access your profile and connect with others.
+          </p>
+          <SignInButton mode="modal">
+            <Button className="w-full" variant="outline">
+              Login
+            </Button>
+          </SignInButton>
+          <SignUpButton mode="modal">
+            <Button className="w-full mt-2" variant="default">
+              Sign Up
+            </Button>
+          </SignUpButton>
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
 
 export default Sidebar;
